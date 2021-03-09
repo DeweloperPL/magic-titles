b812cba6a0b738ea094487c8eeb28792535ab4bf
DeweloperPL <80275928+DeweloperPL@users.noreply.github.com>
2021-03-09 08:59:27 +0100

    Add files via upload

---
+++ magic titles code/MAGIC TITLES.py
@@ -0,0 +1,161 @@
+import  time , random
+inputHASLO = input("Podaj klucz gry :")
+if inputHASLO == str ("admin1234"):
+    print("Klucz aktywowany")
+
+if inputHASLO == str ("githubtest"):
+    print("Klucz aktywowany")
+else:
+    print("Zły klucz!")
+    exit()
+print("Witaj w Magic Tiles")
+time.sleep (1)
+input1 = input ("Czy wejść do tajemniczej chaty?")
+if input1 == str ("tak"):
+    print("znalazłeś księgę czarów")
+else:
+    print("Wracasz do domu (koniec gry)")
+    exit()
+time.sleep(0.8)
+print("Babajaga - Chłopcze co tu robisz?")
+time.sleep(0.8)
+print ("TY- Nic ja szukam miłości")
+time.sleep(1.5)
+print ("Babajaga- to se idź!")
+time.sleep(1.5)
+print ("TY- Wal się")
+time.sleep(1.5)
+print ("(bijatyka)")
+time.sleep(1.5)
+input2 = input ("Znalazłeś mapę gdzie idziemy ? Do lasu ciemnośći czy do wilków?")
+if input2 == str ("las ciemności"):
+    print("Znalazłeś kamień więc idziesz do wilków, żeby je zabić")
+else:
+    print ("wilki cię zabijają :(")
+    exit()
+
+input3 = input("Gdzie idziesz ? Na łąkę lub do karczmy")
+if input3 == str ("karczma"):
+    print("napiłeś się wody masz więcej siły")
+    print("idziesz na łąkę i znajdujesz konia")
+else:
+    print("Znalazłeś konia")
+input4 = input("Zaatakował cię jaszczur walczysz?")
+if input4 == str ("tak"):
+    input5 = input("Zgubiłeś kamień czy walczysz pięściami?")
+    if input5 == str ("tak"):
+        print("zabiłeś jaszczura idziesz dalej")
+    else:
+        print("uciekasz")
+else:
+    print("uciekasz do domu")
+time.sleep(0.7)
+print("Dzien 2")
+time.sleep (0.7)
+input6 = input ("Gdzie idziesz ? Do babci, czy idziesz do lasu za łąką")
+if input6 == str ("las"):
+    print ("Znajdujesz Ringo Starra daje tobie pałeczki MOCY!")
+else:
+    print("U babci dostajesz magiczną pomidorową")
+input7 = input ("Zanalazłeś królestwo czy wchodzisz ???")
+if input7 == str ("tak"):
+    print("znalazłeś księżniczkę, która poleciła tobie dać daninę Rydzykowi")
+else:
+    print ("nie wiesz co robić więc idziesz pracować do maca")
+    dswjdnfwj
+time.sleep(0.7)
+print("Dzień 3")
+input8 = input ("Czy dasz daninę Rydzykowi?")
+if input8 == str ("tak"):
+          print("oddajesz wszystko co masz i dziesz na studia robienia burgerów")
+          gggggggg
+else:
+    print("Rydzyk idzie do parafii, a ty ruszasz w dalszą podróż.")
+time.sleep(1)
+print("Znalazłeś staw, napiłeś się, ale jakiś idiota znjduje cię i każe ci zagrać w grę grasz?")
+input9 = input()
+if input9 == str ("tak"):
+    input10 = input ("Losowanko od 1 do 10 ")
+    liczba = random.randint
+    if input10 == str (liczba):
+        print("wygrana")
+    else:
+        print("przegrana")
+else:
+    print("Idziesz dalej i zasypiasz w hotelu")
+time.sleep(0.7)
+print("Dzień 4")
+time.sleep(1)
+print ("Otkryto nowe miasto")
+time.sleep(0.5)
+input11 = input("Znalazłeś van wchodzisz?")
+if input11 == str ("tak"):
+    print("Vanem jedziesz do Stefana który daje tobie vodke mocy!")
+else:
+    print("Jednak jedziesz vanem")
+    time.sleep(0.7)
+    print("Vanem jedziesz do Stefana który daje tobie vodke mocy!")
+input12 = input("Samochód [*] tankujesz? ")
+if input12 == str ("tak"):
+    print("Jedziesz 20 km/h i po 1h jazdy, idziesz odpocząć ")
+else:
+    print("idziesz spać")
+time.sleep(1.5)
+print("Dzień 5")
+time.sleep(1)
+print("Znalazłeś Paulnę (swoją miłość) ale Babajaga z nią ucieka!")
+time.sleep(1)
+if inputHASLO == str ("rusie wirusy"):
+    print("loadnig complete")
+    time.sleep(1.0)
+    print("ZŁY KLUCZ!")
+    exit()
+input13 = input ("Czy użyć czaru (wszystkie fanty znikną tu a miłość zawita znów")
+if input13 == str ("tak"):
+    print("AUU")
+else:
+    input14 = input("Babajaga ucieka razem z miłością. Możesz poprosić o pomoc silnego przchodnia z dziewczyną, zrobić to?")
+    if input14 == str ("tak"):
+        print("Przechodzień zabija babajage a ty tracisz przytomność!")
+    else:
+        print("Wracasz do domu...")
+        exit(code)
+time.sleep(2)
+print("dzień 6")
+time.sleep (0.5)
+print("Budzisz się w domu wchodzisz do kuchni i widzisz Paulnię.")
+time.sleep(0.5)
+print("Całujesz się z Pauliną i żyjecie długo i szczęśliwie...")
+time.sleep(0.5)
+print("Rozdział drugi - o północy")
+time.sleep(0.5)
+print("Dom godzina 3:00 - szłyszysz hałas" )
+time.sleep(0.5)
+input15 = input ("Czy idziesz sprawdzić co to?")
+if input15 == str ("tak"):
+    print ("Widzisz rozbite okno nie widząc za dużo podchodzisz do niego")
+    print("obok rozbitego okna jest porywacz z twoją dziewczyną")
+else:
+    print("zasypiasz ale coś nie daje ci zasnąć")
+    time.sleep(0.5)
+    print("budzi cię znowu jakiś hałas i krzyk CHORELRA JASNA!")
+    input16= input("czy dalej hcesz spać?")
+    if input16 == str ("tak"):
+        print ("wdzisz swoją ukochaną w worku na ziemniaki")
+    else:
+        print("zasypiasz i tajemniczy porywacz cię porywa")
+        time.sleep(0.5)
+        exit()
+time.sleep(0.5)
+time.sleep(0.5)
+print ("widzisz otwarte okno i słyszysz pisk opon")
+time.sleep(0.5)
+print("widzisz zielony, duży van z napisamem #pozdro")
+time.sleep(1)
+input17 = input("chcesz wziąść auto ale masz piżamę przebierasz się?")
+if input17== str ("nie"):
+    print("szybko wybiegasz biorąc kluczyki do auta")
+    time.sleep(1.0)
+    print("auto szybko odpala i jedziesz w pościg ")
+    time.sleep(1.0)
+print("THE END wersja : 0.3")
